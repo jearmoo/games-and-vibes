d7caca20c2fdb9623efa771a27cdf14dc7f90363
Saransh Kacharia <saranshis@hotmail.com>
2026-04-01 13:02:20 -0700

    Updated Title
    Test GitHub Action

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -47,7 +47,7 @@ function Hero() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
       >
-        JERPI GAMES
+        GAMES AND VIBES
       </motion.h1>
 
       <motion.p
